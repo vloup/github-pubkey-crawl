85071292e4920f5ebca06b40791d9aec40518404
vloup <vincent.loup@epfl.ch>
2017-09-18 12:33:16 +0200

    Only loop when the API call is exhausted.

--- crawl.sh
+++ crawl.sh
@@ -2,11 +2,11 @@
 
 dub build
 
-retcode=-1
-while [ "$retcode" -ne 0 ]; do
+retcode=254
+while [ "$retcode" -eq 254 ]; do
 	./github-pubkey-crawl -w 20
 	retcode=$?
-	if [ "$retcode" -ne 0 ]; then
+	if [ "$retcode" -eq 254 ]; then
 		echo "API Limit reached, waiting for an hour."
 		sleep 1h
 	fi
